6fe11a708c2bcb18cc57fd775567d6a38150ce6c
unknown <filip.vasic2000@gmail.com>
2022-07-05 21:25:21 +0200

    update service

--- crypto-project/src/app/components/currency-list/currency-list.component.ts
+++ crypto-project/src/app/components/currency-list/currency-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { CryptoModel } from 'src/app/model/crypto';
 import { CryptoServiceService } from 'src/app/services/crypto-service.service';
 
@@ -10,39 +10,12 @@ import { CryptoServiceService } from 'src/app/services/crypto-service.service';
 })
 export class CurrencyListComponent implements OnInit {
 
-  currencies !: CryptoModel[]
+  currencies !: Observable<CryptoModel[]>
 
-  constructor(private currencyServic : CryptoServiceService) { }
+  constructor(private currencyService : CryptoServiceService) { }
 
   ngOnInit(): void {
-    this.currencies = [
-      {
-        "id": "bitcoin",
-        "rank": "1",
-        "symbol": "BTC",
-        "name": "Bitcoin",
-        "supply": "17193925.0000000000000000",
-        "maxSupply": "21000000.0000000000000000",
-        "marketCapUsd": "119150835874.4699281625807300",
-        "volumeUsd24Hr": "2927959461.1750323310959460",
-        "priceUsd": "6929.82",
-        "changePercent24Hr": "-0.81",
-        "vwap24Hr": "7175.06"
-      },
-      {
-        "id": "ethereum",
-        "rank": "2",
-        "symbol": "ETH",
-        "name": "Ethereum",
-        "supply": "101160540.0000000000000000",
-        "maxSupply": null,
-        "marketCapUsd": "40967739219.6612727047843840",
-        "volumeUsd24Hr": "1026669440.6451482672850841",
-        "priceUsd": "404.97",
-        "changePercent24Hr": "-0.09",
-        "vwap24Hr": "415.32"
-      }
-    ]
+   this.currencyService.getAssets().subscribe(c => this.currencies = of(c.data));
   }
 
 }
--- crypto-project/src/app/services/crypto-service.service.ts
+++ crypto-project/src/app/services/crypto-service.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { AssetsModel } from '../model/assets';
 import { CryptoModel } from '../model/crypto';
 
@@ -13,9 +13,43 @@ export class CryptoServiceService {
 
   HOST = "https://api.coincap.io/v2/";
 
+  currencies = [
+    {
+      "id": "bitcoin",
+      "rank": "1",
+      "symbol": "BTC",
+      "name": "Bitcoin",
+      "supply": "17193925.0000000000000000",
+      "maxSupply": "21000000.0000000000000000",
+      "marketCapUsd": "119150835874.4699281625807300",
+      "volumeUsd24Hr": "2927959461.1750323310959460",
+      "priceUsd": "6929.82",
+      "changePercent24Hr": "-0.81",
+      "vwap24Hr": "7175.06"
+    },
+    {
+      "id": "ethereum",
+      "rank": "2",
+      "symbol": "ETH",
+      "name": "Ethereum",
+      "supply": "101160540.0000000000000000",
+      "maxSupply": null,
+      "marketCapUsd": "40967739219.6612727047843840",
+      "volumeUsd24Hr": "1026669440.6451482672850841",
+      "priceUsd": "404.97",
+      "changePercent24Hr": "-0.09",
+      "vwap24Hr": "415.32"
+    }
+  ]
+  
   constructor(private httpClient : HttpClient) { }
 
-  public getCryptos() : Observable<AssetsModel[]> {
-    return this.httpClient.get<AssetsModel[]>(this.HOST + "assets");
+  public getCryptos() : Observable<CryptoModel[]> {
+    this.httpClient.get<AssetsModel>(this.HOST + "assets")
+    return of(this.currencies)
+  }
+
+  public getAssets() : Observable<AssetsModel> {
+    return this.httpClient.get<AssetsModel>(this.HOST + "assets");
   }
 }
